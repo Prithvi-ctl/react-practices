bf4819e038d0937ab155d00542b53439611ab6ba
Prithvi Ghale <prithvighale616@gmail.com>
2026-03-12 13:18:13 +0545

    fisher yates algorithm

--- sudoku/src/App.jsx
+++ sudoku/src/App.jsx
@@ -1,13 +1,31 @@
 import {useState,useEffect} from 'react';
 
 
+function randomizer(){
+  const nums = [1,2,3,4,5,6,7,8,9];
+  for(let i= nums.length-1;i>  0;i--){
+  const j = Math.floor(Math.random() * (i+1));
+  [nums[i],nums[j]] = [nums[j],nums[i]];
+}
+return nums;
+
+}
+
+
 function Boxes(){
+  const numeros = randomizer();
     return(
       <div className="grid grid-cols-3 w-fit">
-        {Array.from({length:9}).map((_,i) =>(
-          <div key={i} className="w-24 h-24 border-2 border-black"></div>
-        ))}
+       {numeros.map((num,i) =>(
+        <div key={i}
+        className="w-24 h-24 border-2 border-black flex justify-center items-center text-4xl">
+          {num}
+          </div>
+          
+       ))}
+       
       </div>
+      
 
 
     )
@@ -17,11 +35,15 @@ function Board(){
   
 
   return(
-  <div className="grid grid-cols-3 gap-0 w-fit flex">
+    <>
+    <div className="flex justify-center items-center h-screen">
+  <div className="grid grid-cols-3 gap-0 w-fit">
     {Array.from({length:9}).map((_,i)=>(
-      <Boxes key={i}/>
+      <Boxes key={i} />
     ))}
   </div>
+  </div>
+  </>
   )}
 
 
